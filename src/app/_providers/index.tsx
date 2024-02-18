c3c40382059a3e4678d0978e9f44721bd372aa10
Keith Maibach <keith@laterolabs.ca>
2024-02-18 11:49:46 -0800

    add chakra UI

--- src/app/_providers/index.tsx
+++ src/app/_providers/index.tsx
@@ -4,16 +4,19 @@ import React from 'react'
 
 import { AuthProvider } from '../_providers/Auth'
 import { CartProvider } from '../_providers/Cart'
-import { ThemeProvider } from './Theme'
+// import { ThemeProvider } from './Theme'
+import { ChakraProvider } from '@chakra-ui/react'
 
 export const Providers: React.FC<{
   children: React.ReactNode
 }> = ({ children }) => {
   return (
-    <ThemeProvider>
+    <ChakraProvider>
+      {/* <ThemeProvider> */}
       <AuthProvider>
         <CartProvider>{children}</CartProvider>
       </AuthProvider>
-    </ThemeProvider>
+      {/* </ThemeProvider> */}
+    </ChakraProvider>
   )
 }
